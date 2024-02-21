0179201127097127a70957b5cbbc7bf5a59e64b3
nodirbek <suyunovnodir22@gmail.com>
2024-02-21 15:28:50 +0500

    video lesson

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,3 +5,6 @@ from django.views.generic import CreateView
 # Create your views.py here.
 class RegisterView(CreateView):
     print('Test verions')
+
+class LoginView(RegisterView):
+    print('test 2yuborildi')
